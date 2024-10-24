cd8fe7be7510bb11c82f1593d891a98c3393092f
Jane Holborn <janeholborn@hotmail.com>
2024-10-24 17:17:55 +0800

    Create basic UI and placeholder text. Create method to add tasks.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,44 @@ namespace WPFToDoListApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<string> Tasks { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Tasks = new ObservableCollection<string>();
+            TaskListBox.ItemsSource = Tasks;
+            TextBoxAdd.Foreground = Brushes.Gray;
+        }
+
+        // This method is used to clear the placeholder text from the add task text box and make the text visible when adding a task.
+        private void TextBoxAdd_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxAdd.Text == "Type your task here.")
+            {
+                TextBoxAdd.Clear();
+                TextBoxAdd.Foreground = Brushes.Black;
+            }
+        }
+
+        // This method is used to add a new task.
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TextBoxAdd.Text) && TextBoxAdd.Text != "Type your task here.")
+            {
+                Tasks.Add(TextBoxAdd.Text);
+                TextBoxAdd.Clear();
+                TextBoxAdd_LostFocus(null, null);
+            }
+        }
+
+        // This method is used to add the placeholder text back in if the user clicks away and has not started adding a task.
+        private void TextBoxAdd_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxAdd.Text == string.Empty)
+            {
+                TextBoxAdd.Text = "Type your task here.";
+                TextBoxAdd.Foreground = Brushes.Gray;
+            }
         }
     }
 }
